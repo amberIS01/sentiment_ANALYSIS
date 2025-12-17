804f9d4c24d83ff534eb2f61289bc6f77e6a263b
Sahil Singh <sahilsingh8300@gmail.com>
2025-12-17 07:14:37 +0530

    Add utils package init

---
+++ chatbot/utils/__init__.py
@@ -0,0 +1,13 @@
+"""
+Utilities Package
+
+Common utility functions and helpers.
+"""
+
+from chatbot.utils import truncate_text, format_duration, safe_divide
+
+__all__ = [
+    "truncate_text",
+    "format_duration",
+    "safe_divide",
+]
